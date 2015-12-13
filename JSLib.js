77cdb29eb9005bbc65f54c587da27e993f6ffea4
NaijaProgrammer <orji4y@yahoo.com>
2015-12-13 23:35:50 +0100

    Cleaning up the library : Removed some "unnecessary" functions

--- JSLib.js
+++ JSLib.js
@@ -1,211 +1,3 @@
-var MO = MO || {};
-MO.namespace = function(name)
-{
-	var parts   = name.split('.');
-	var current = MO;
-	for (var i in parts)
-	{
-		if (!current[parts[i]])
-		{
-			current[parts[i]] = {};
-		}
-		current = current[parts[i]];
-	}
-}
-
-/*
-* @credits: Wrox professional javascript for web developers, pg 197
-*/
-if (typeof Node == "undefined") 
-{
-	var Node = {
-		ELEMENT_NODE                : 1,
-		ATTRIBUTE_NODE              : 2,
-		TEXT_NODE                   : 3,
-		CDATA_SECTION_NODE          : 4,
-		ENTITY_REFERENCE_NODE       : 5,
-		ENTITY_NODE                 : 6,
-		PROCESSING_INSTRUCTION_NODE : 7,
-		COMMENT_NODE                : 8,
-		DOCUMENT_NODE               : 9,
-		DOCUMENT_TYPE_NODE          : 10,
-		DOCUMENT_FRAGMENT_NODE      : 11,
-		NOTATION_NODE               : 12
-	}
-}
-
-
-/*
-* @Credits: http://www.javascriptkit.com/javatutors/loadjavascriptcss.shtml
-* @date; July 13, 2011; 14:24
-* @modified by: Michael Orji
-*/
-var filesadded="" //list of files already added
-
-/*
-* Determines if file already exists in the page
-*/
-function fileExists(filename, filetype)
-{
-	if (filesadded.indexOf( "[" + filename + "@" + filetype + "]" ) == -1)
-	{
-		filesadded += "[" + filename + "@" + filetype + "]," //List of files added in the form "[filename1@js],[filename2@css],etc" I -- (Michael O.) -- added the @ symbol and the comma(,) incase a situation arises where I need to split() the string into an array
-		return false;
-	}
-	return true;
-}
-
-/*
-* removes a file from the page
-* Arguments:
-*   String name of file
-*   String type of file
-* @credits: http://www.javascriptkit.com/javatutors/loadjavascriptcss2.shtml
-* @date: Feb 28, 2012
-* @modified by: Michael Orji
-*/
-function removeFile(filename, filetype){
- var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
- var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
- var allsuspects=document.getElementsByTagName(targetelement)
-   for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
-      if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1){
-       allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-      }
-   }
-}
-
-/*
-* Dynamically loads a server-side (php) script into the current page, script must return valid javascript 
-* Arguments: 
-*    Array files to add, 
-*    boolean specifying whether to overwrite the file if it already exists in the page
-* Not to be called directly, as it is used by the $_JS function internally
-* @date: March 13, 2012
-*/
-function $PHP_JS(JSPHPFilesArray, overWrite){
-
-   var str = ''; 
- 
-   for(var i in JSPHPFilesArray){ 
-      
-      if(overWrite){
-         if(fileExists(JSPHPFilesArray[i], "php") ){ 
-          removeFile(JSPHPFilesArray[i], "php");
-         }
-         if(JSPHPFilesArray[i].indexOf('.php') == -1){
-            JSFilesArray[i] += '.php';
-          } 
-      }
-
-      if( !fileExists(JSPHPFilesArray[i], "php") ){ //call the fileExists function to include the current 'js' file in the 'filesadded' string
-          if(JSPHPFilesArray[i].indexOf('.php') == -1){
-            JSPHPFilesArray[i] += '.php';
-          } 
-      } 
-
-     var JSLink = document.createElement("script");
-     JSLink.setAttribute("type", "text/javascript");
-     JSLink.setAttribute("src", JSPHPFilesArray[i]);
-
-      if(typeof JSLink != "undefined"){
-       document.getElementsByTagName("head")[0].appendChild(JSLink);
-      } 
-   }
-}
-
-/*
-* Dynamically loads a javascript file into the current page,
-* Arguments: 
-*   array  files to add, 
-*   boolean specifying whether to overwrite the file if it already exists in the page, 
-*   boolean specifying if the file is a serverside (php) script
-* @author: michael orji
-* @date: Oct 2, 2010
-* @modified: Feb 28, 2012
-*/
-function $JS(JSFilesArray, overWrite, isPHPFile){
-
-   if(isPHPFile){
-    $PHP(JSFilesArray, overWrite);
-    return;
-   }
-
- var str = ''; 
- 
-   for(var i in JSFilesArray){ 
-      
-      /*
-      * if the file already exists (in the 'filesadded' string) and we're overwriting the file in the document tree, 
-      * we pull it out from the document tree,  
-      * then re-insert it into the tree, this way, we remain consistent in the 'filesadded' string since we don't alter (i.e remove it from)
-      * the string when we remove the file from the document tree,
-      */
-      if(overWrite){
-         if(fileExists(JSFilesArray[i], "js") ){ 
-          removeFile(JSFilesArray[i], "js");
-         }
-         if(JSFilesArray[i].indexOf('.js') == -1){
-            JSFilesArray[i] += '.js';
-          } 
-      }
-
-      if( !fileExists(JSFilesArray[i], "js") ){ //call the fileExists function to include the current 'js' file in the 'filesadded' string
-          if(JSFilesArray[i].indexOf('.js') == -1){
-            JSFilesArray[i] += '.js';
-          } 
-      } 
-
-     var JSLink = document.createElement("script");
-     JSLink.setAttribute("type", "text/javascript");
-     JSLink.setAttribute("src", JSFilesArray[i]);
-
-      if(typeof JSLink != "undefined"){
-       document.getElementsByTagName("head")[0].appendChild(JSLink);
-      } 
-   }
-}
-
-
-/*
-* dynamically loads css stylesheets
-* @argument - array of css files to load
-* @author: michael orji
-* @date: sept 29, 2010
-* based on ajax_im's ajax_im.js
-*/
-function $CSS(cssFilesArray){
-   
-   for(var i in cssFilesArray){
-      if( !fileExists(cssFilesArray[i], "css") ){
-       var CSSLink = document.createElement("link");
-       CSSLink.setAttribute("rel", "stylesheet");
-       CSSLink.setAttribute("type", "text/css");
-
-         if(cssFilesArray[i].indexOf('.css') == -1){
-          cssFilesArray[i] += '.css';
-         }
-   
-       CSSLink.setAttribute("href", cssFilesArray[i]);
-     
-         if(typeof CSSLink != "undefined"){
-          document.getElementsByTagName("head")[0].appendChild(CSSLink);
-         }
-      }
-   }
-}
-
-
-/*
-* Wrapper function for document.getElementsByTagName
-* Argument: HTML element tag name
-*/
-function $Tag(tagName)
-{
-	var that = (this == window) ? document : this;
-	return ( (tagName) ? that.getElementsByTagName(tagName) : that.getElementsByTagName('*') );
-}
-
 /*
 * Wrapper function for document.getElementById
 * Argument: HTML element id or DOM object reference
@@ -239,12 +31,47 @@ function $O(elem)
 }
 
 /*
+* Wrapper function for document.getElementsByTagName
+* Argument: HTML element tag name
+*/
+function $Tag(tagName)
+{
+	var that = (this == window) ? document : this;
+	return ( (tagName) ? that.getElementsByTagName(tagName) : that.getElementsByTagName('*') );
+}
+
+/*
 * returns the style property of supplied element
 * Argument: HTML element id or reference
+*
+* @added style opts on Dec. 13, 2015 @9:56pm
 */
-function $Style(elem)
+function $Style(elem, styleOpts)
 {
-	return $O(elem).style;
+	elem = $O(elem);
+	if(!elem)
+	{
+		return null;
+	}
+	if(typeof styleOpts === 'string')
+	{
+		optsArr = styleOpts.split(',');
+		optsArr.forEach(function(member){
+			var memberArr  = member.split('=');
+			var styleName  = memberArr[0];
+			var styleValue = memberArr[1];
+			elem.style['styleName'] = String(styleValue);
+		});
+	}
+	else if(typeof styleOpts === 'object')
+	{
+		for(var x in styleOpts)
+		{
+			elem.style[x] = String(styleOpts[x]);
+		}
+	}
+	
+	return elem.style;
 }
 
 /* 
@@ -363,14 +190,15 @@ function $properties(element)
 * centers an element on the page
 * @author: Michael Orji
 * @date: August 25, 2012
+* @dependency : Browser
 */
 function $Center(elem)
 {
 	elem = $O(elem);
 
 	var elemDimensions = size(elem);
-	var elemWidth      =  parseInt(elemDimensions.width)  || parseInt(getStyleValue(elem, 'width')); 
-	var elemHeight     =  parseInt(elemDimensions.height) || parseInt(getStyleValue(elem, 'height'));
+	var elemWidth      = parseInt(elemDimensions.width)  || parseInt(getStyleValue(elem, 'width')); 
+	var elemHeight     = parseInt(elemDimensions.height) || parseInt(getStyleValue(elem, 'height'));
 	var bDimensions    = Browser.Window.size();
 	var bWidth         = parseInt(bDimensions.width);
 	var bHeight        = parseInt(bDimensions.height);
@@ -407,15 +235,6 @@ function trim(s)
 	return ( (typeof s == 'string') ? s.replace(/(^\s+)|(\s+$)/g, "") : s );
 }
 
-/* 
-* remove leading and trailing spaces from string
-* @credits: by joshua gross (popup.html of ajax_im )
-*/
-function trim2(text)
-{
-	return (text != null) ? text.replace(/^[ \t]+|[ \t]+$/g, "") : null;
-}
-
 /*
 * @credits: Stoyan Stefanov "Object Oriented Javascript"
 */
@@ -445,6 +264,8 @@ function extend(Child, Parent)
 * it has the advantage that the child does not inherit the parent's non-reference properties, and hence cannot modify them
 * thereby inheriting only reference properties which are meant to be re-used
 *
+* Based on the customExtend function by Stoyan Stefanov in Object Oriented Javascript
+*
 * USE EXAMPLES:
 * 1. var childObject = referenceExtend(parentObject); //create a brand new Child Object with no properties except parent's reference members
 * 2. var childObject = { 'prop':'value', 'method':methodDefinition }
@@ -462,7 +283,6 @@ function referenceExtend(Parent, Child)
 			if ( (typeof Parent[i] === 'object') || (typeof Parent[i] === 'array') )
 			{
 				Child[i] = (Parent[i].constructor === Array) ? [] : {};
-				//customExtend(Parent[i], Child[i]);
 				referenceExtend(Parent[i], Child[i]);
 			}
 			else 
@@ -473,17 +293,6 @@ function referenceExtend(Parent, Child)
 	}
 	
 	return Child;
-}
-
-/*
-* @date: Oct. 10, 2012, 20:37
-*/
-function logToConsole(msg)
-{
-	if(typeof console != 'undefined' && typeof console.log == 'function')
-	{
-		return console.log(msg);
-	}
 }
 
 /*
@@ -529,6 +338,7 @@ function getPath(targetName)
 }
 
 /*
+* get the document height
 * credit: http://james.padolsey.com/javascript/get-document-height-cross-browser/
 */
 function docHeight()
@@ -723,16 +533,6 @@ function toggleDisplay(elem, currDisplay)
 	return elem;
 }
 
-function setStatus(elem, statusIndicator)
-{
-	$Html(elem, statusIndicator);
-}
-
-function clearStatus(elem)
-{
-	$Html(elem, '');
-}
-
 function resetForm(theform)
 {
 	$O(theform).reset();
@@ -866,29 +666,6 @@ function pageName()
 }
 
 /*
-* @ author: Michael Orji
-* @ date: Sept 20, 2012
-*/
-function setDisplayStringLength(str, maxLength, padXter)
-{
-	padXter = padXter || '...'
-	return ( str.substring(0, maxLength) + (str.length > maxLength ?  padXter : '') );
-}
-
-function getLastChild(parent, excludeNodeType)
-{
-	excludeNodeType = excludeNodeType || 3;
-	var lastBorn = parent.lastChild;
-
-	while(lastBorn.nodeType == excludeNodeType)
-	{
-		lastBorn = lastBorn.previousSibling;
-	}
-	
-	return lastBorn;
-}
-
-/*
 * original code source: http://www.quirksmode.org/js/findpos.html 
 * access date: July 9, 2011, 10:42 am;
 */
@@ -924,29 +701,6 @@ function getElemPosition(theElement)
 	return {'left': positionLeft, 'top': positionTop};
 }
 
-//Opening Off-site Links in a New Window
-/*
-USAGE : 
-all external links should have the rel="external" attribute
-1. SITE: <a href="http://www.google.com/" rel="external">Google (offsite)</a>
-2. document.onclick = openExtLinks;
-*/
-openExtLinks = function(e)
-{
-	var target = e ? e.target : window.event.srcElement;
-
-	while (target && !/^(a|body)$/i.test(target.nodeName))
-	{
-		target = target.parentNode;
-	}
-
-	if (target && target.getAttribute('rel') && target.rel == 'external')
-	{
-		var external = window.open(target.href);
-		return external.closed;
-	}
-}
-
 function setElemSize(elem, w, h)
 {
 	elem = $O(elem);
@@ -954,11 +708,9 @@ function setElemSize(elem, w, h)
 	$Style(elem).height = (String(h).replace('px', '')) + 'px';
 }
 
-function changeLocation(url)
-{
-	location.href = url;
-}
-
+/*
+* Create a new <option> element for a <select> element
+*/
 function createMenuItem (value, label)
 {
 	var newOpt       = document.createElement("option");
